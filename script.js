793a62a0087ae350f6bdc2c5cf77c07d8435651b
Nishant Rajput <nishant.rajput0502@gmail.com>
2026-09-11 05:48:11 +0530

    Playing around with promises even more

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
-const cart = ["shoes","pants","kurta"];
-
+const cart = ["shirt","pant","jeans"];
 createOrder(cart)
 .then(function(orderId){
-    console.log(orderId);
+    console.log("Order Id : "+ orderId);
     return orderId;
 })
-.catch(function(err){
-    console.log("Failed : "+ err.message);
-})
 .then(function(orderId){
-    console.log("Inside the proceed to payment  : "+ orderId);
-    return proceedToPayment(orderId);
+    // ab order id aagyi hai
+    return proceedtoPayment(orderId);
+    // proceed to payment hai ab
 })
-.then(function(paymentInfo){
-    console.log(typeof(paymentInfo));
-    console.log(paymentInfo);
+.then(function(payment){
+    console.log("Order Summary : ");
+    console.log(payment);
+    const balance = 500;
+    return balance;
+})
+.then(function(balance){
+    return updateBalance(balance);
+})
+.then(function(res){
+    console.log("Final status : "+ res);
+})
+.catch(function(err){
+    console.log("Error : "+err.message);
 })
 
+// ye ek api banani hai hame
+
 function createOrder(cart){
-    const pr = new Promise(function(resolve,reject){
+    return new Promise(function(resolve,reject){
         if(!validateCart(cart)){
-            const err = new Error("Cart not valid");
+            const err = new Error("Invalid Cart");
             reject(err);
         }
-        const orderId = "12345";
-        if(orderId)
-        {
-            
-            resolve(orderId);
-        }
-    })
-    return pr;
+        const orderId = '123';
+        resolve(orderId);
+    });
 }
-function proceedToPayment(orderId){
+function validateCart(cart){
+    return true;
+}
+function proceedtoPayment(orderId){
     return new Promise(function(resolve,reject){
         resolve("Payment Successful");
     })
 }
-
-function validateCart(cart){
-    return false;
+function updateBalance(balance){
+    const pr = new Promise(function(resolve,reject){
+        if(balance<0){
+            reject("Insufficient balance");
+        }
+        setTimeout(function(){
+            const curr = document.getElementById("balance");
+            curr.innerText = 500;
+            resolve("Balance updated successfully");
+        },5000);
+    })
+    return pr;
 }
